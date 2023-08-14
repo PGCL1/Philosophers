bed4359a2efb8b94cbbe7d1c65bafac61ac52dbc
Guillaume Lacroix <glacroix@c1r14s3.42madrid.com>
2023-08-14 17:59:48 +0200

    practice + learning folder created

---
+++ learning/cv_passingargs0.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+
+//create 10 threads each printing a unique prime number from the array and printing it to the screen
+int arr[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+pthread_mutex_t mutex;
+
+void* routine(int *num){
+
+	pthread_mutex_lock(&mutex);
+	printf("%d ", *num);
+	pthread_mutex_unlock(&mutex);
+	free(num);
+	return NULL;
+}
+
+int main()
+{
+	pthread_t thread[10];
+	pthread_mutex_init(&mutex, NULL);
+	//creating each thread
+	/* for (int i = 0; i < 10; i++)
+		pthread_create(thread + i, NULL, (void*) routine, arr + i); */
+	//or do this
+	for (int i = 0; i < 10; i++)
+	{
+		int *num = malloc(sizeof(int));
+		*num = arr[i];
+		pthread_create(thread + i, NULL, (void *)routine, num);
+	}
+
+	//waiting for the execution to end for each thread
+	for (int i = 0; i < 10; i++)
+		pthread_join(thread[i], NULL);
+	pthread_mutex_destroy(&mutex);
+	return (0);
+}
+
+
+/*
+void* routine(void* num){
+	int	i = *(int*)num;
+	pthread_mutex_lock(&mutex);
+	printf("Thread is printing %d\n", arr[i]);
+	pthread_mutex_unlock(&mutex);
+
+	return NULL;
+}
+
+int main()
+{
+	pthread_t thread[10];
+	pthread_mutex_init(&mutex, NULL);
+	//creating each thread
+	for (int i = 0; i < 10; i++)
+	{
+		int	*id = malloc(sizeof(*id));
+//		*id = i;
+		pthread_create(thread + i, NULL, &routine, id);
+	}
+
+	//waiting for the execution to end for each thread
+	for (int i = 0; i < 10; i++)
+		pthread_join(thread[i], NULL);
+	pthread_mutex_destroy(&mutex);
+	return (0);
+}
+
+*/
